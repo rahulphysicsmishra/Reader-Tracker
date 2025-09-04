7339bd8fde8c08e3aa30c978d5f7216524ae2182
rahul mishra <rahulphysicsmishra@gmail.com>
2025-09-04 15:53:37 +0530

    saved screen code added, books visible

--- lib/pages/saved_screen.dart
+++ lib/pages/saved_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:reader_tracker/db/database_helper.dart';
+import 'package:reader_tracker/models/book.dart';
 
 class SavedScreen extends StatefulWidget {
   const SavedScreen({super.key});
@@ -13,6 +15,23 @@ class _SavedScreenState extends State<SavedScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Saved"),
+      ),
+      body: FutureBuilder<List<Book>>(
+        future: DatabaseHelper.instance.readAllBooks(),
+        builder: (context, snapshot) => snapshot.hasData ? ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            final book = snapshot.data![index];
+            return ListTile(
+              leading: book.imageLinks.isNotEmpty
+                  ? Image.network(book.imageLinks['thumbnail'] ?? '')
+                  : null,
+              title: Text(book.title),
+              subtitle: Text(book.authors.isNotEmpty ? book.authors.join(', ') : 'Unknown Author'),
+            );
+          },
+        ) : const Center(child: CircularProgressIndicator()),
+        
       ),
     );
   }
